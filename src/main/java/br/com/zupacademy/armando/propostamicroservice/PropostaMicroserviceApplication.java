9a3b459a4c91ef5ff9e727826886324b5af81a19
Armando Tadeu <atdc.codemaster@gmail.com>
2021-06-02 15:44:40 -0300

    initial setup

---
+++ src/main/java/br/com/zupacademy/armando/propostamicroservice/PropostaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.armando.propostamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/armando/propostamicroservice/PropostaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.armando.propostamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
